8e7605761121ee90d724eab7ef46a1a99ecd5cce
Haichao Hu <huhaichaotd@139.com>
2024-11-07 19:05:21 +0800

    Simon游戏，初始版本

---
+++ simon.js
@@ -0,0 +1,26 @@
+/**
+ * Simon游戏的按钮交互逻辑
+ * 为所有游戏按钮添加点击效果
+ * 
+ * @listens click - 监听.btn元素的点击事件
+ * @requires jQuery
+ */
+$(".btn").on("click", function() {
+    /**
+     * 被点击的按钮jQuery对象
+     * @type {jQuery}
+     */
+    const $btn = $(this);
+    
+    // 添加激活状态的class，触发按钮按下的视觉效果
+    $btn.addClass("btn-active");
+    
+    /**
+     * 延时移除按钮激活状态
+     * @param {Function} callback - 移除btn-active类的回调函数
+     * @param {number} delay - 延迟时间（毫秒）
+     */
+    setTimeout(() => {
+        $btn.removeClass("btn-active");
+    }, 200);
+});
